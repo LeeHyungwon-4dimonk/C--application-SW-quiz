564a174a9924ab95ef59308b036afbc0ef480427
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-03-21 12:55:18 +0900

    [feat]FindKeyIndex
    주어진 문자열 중 문자가 있는 인덱스를 구하는 함수 작성

--- C#ApplicationQuiz/C#ApplicationQuiz/Program.cs
+++ C#ApplicationQuiz/C#ApplicationQuiz/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        // 주어진 문자열 중 문자가 있는 인덱스를 구하는 함수를 작성하시오. 단 없을 경우 -1을 출력하시오.
+
+        // 입력 : "abcdef", 'd'
+        // 출력 : 3
+
+        // 입력 : "Winner winner chicken dinner", 'c'
+        // 출력 : 14
+
+        // 입력 : "Not your lucky day", 'p'
+        public static int FindKeyIndex(string text, char key)
+        {
+            char[] array = text.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (key == array[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(FindKeyIndex("abcdef", 'd'));
         }
     }
 }
